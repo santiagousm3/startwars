39c2acadb401bd061be932fd7e2cb419158a2701
Yeynny Barco <ybarco@csmsa.co>
2020-04-03 16:02:16 -0500

    Las imagenes completas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './pages/menu/menu.component';
+import { PeliculasComponent } from "./pages/peliculas/peliculas.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: MenuComponent},
+{path: 'peliculas', component: PeliculasComponent},
+{path: '**', pathMatch: 'full', redirectTo: ''}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Angular2SwapiModule } from 'angular2-swapi';
+//Rutas
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MenuComponent } from './pages/menu/menu.component';
+import { PeliculasComponent } from "./pages/peliculas/peliculas.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MenuComponent
+    MenuComponent,
+    PeliculasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Angular2SwapiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
